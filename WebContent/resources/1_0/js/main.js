0ef62183e07d981c5c8d679fa084f23ca822c531
Kris Clark <kclark@actuate.com>
2015-06-15 15:10:57 -0500

    Removed client side code mirror, bootstrap, and other external client side resources.  New UI using primefaces

--- WebContent/resources/1_0/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {	
-	// Check if user is logged in
-	if($.cookie('actToken') == null) {
-	}
-	
-	$('.rptdesignTitle').html('new report ' + new Date($.now()) + '.rptdesign');
-});
--- WebContent/resources/1_0/js/xmlArea.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var editor = CodeMirror.fromTextArea(document.getElementById("xmlText"), {
-        lineNumbers: true,
-        smartIndent: true,
-        indentWithTabs: true,
-        mode: "xml"
-    });
-})
